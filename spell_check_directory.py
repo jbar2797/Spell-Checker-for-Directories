ba3fdbf2aa22d102053b01ecbf89882287e0d380
Jonathan Barreneche <47704964+jbar2797@users.noreply.github.com>
2023-08-14 10:44:20 -0700

    Update spell_check_directory.py
    Updated script to v2.0

--- spell_check_directory.py
+++ spell_check_directory.py
@@ -1,79 +1,88 @@
 import os
+import argparse
 from spellchecker import SpellChecker
 from tqdm import tqdm
-import chardet
+import json
+from git import Repo
 
-def detect_file_encoding(file_path):
-    with open(file_path, 'rb') as f:
-        result = chardet.detect(f.read())
-        return result['encoding']
+def backup_file(file_path):
+    backup_path = file_path + '.bak'
+    with open(file_path, 'rb') as original, open(backup_path, 'wb') as backup:
+        backup.write(original.read())
 
-def check_spelling_in_file(file_path):
+def check_and_correct_spelling(file_path, custom_dict):
     spell = SpellChecker()
-    encoding = detect_file_encoding(file_path)
+    spell.word_frequency.load_words(custom_dict)
     errors = {}
 
-    with open(file_path, 'r', encoding=encoding, errors='replace') as f:
-        for line_num, line in enumerate(f, 1):
-            words = line.split()
-            misspelled = spell.unknown(words)
-            if misspelled:
-                context = line.strip()
-                for word in misspelled:
-                    errors[word] = (line_num, context)
+    with open(file_path, 'r', errors='replace') as f:
+        lines = f.readlines()
+
+    for line_num, line in enumerate(lines, 1):
+        words = line.split()
+        misspelled = spell.unknown(words)
+        if misspelled:
+            for word in misspelled:
+                print(f"Misspelled word '{word}' found in {file_path} on line {line_num}: {line.strip()}")
+                action = input("Action [ignore (i), correct (c), replace with suggestion (r)]: ")
+                if action == 'c':
+                    correct = input(f"Enter correction for {word}: ")
+                    lines[line_num - 1] = lines[line_num - 1].replace(word, correct)
+                elif action == 'r':
+                    suggestions = spell.candidates(word)
+                    if suggestions:
+                        print(f"Suggestions: {', '.join(suggestions)}")
+                        choice = input("Enter your choice or leave blank to skip: ")
+                        if choice in suggestions:
+                            lines[line_num - 1] = lines[line_num - 1].replace(word, choice)
+
+    with open(file_path, 'w', errors='replace') as f:
+        f.writelines(lines)
 
     return errors
 
 def main():
-    folder = input("Please enter the folder path: ")
-    ext_filter = input("Enter file extensions to check (comma separated, e.g. .txt,.md) or leave blank for all: ").split(',')
-    scan_subdirectories = input("Do you want to scan subdirectories as well? (yes/no): ").lower().strip() == 'yes'
+    parser = argparse.ArgumentParser(description="Spell Checker for Directories")
+    parser.add_argument('--path', type=str, default='.', help='Directory path to check')
+    parser.add_argument('--extensions', type=str, default='.txt', help='Comma separated file extensions to check')
+    parser.add_argument('--subdirs', action='store_true', help='Include subdirectories')
+    parser.add_argument('--config', type=str, default='config.json', help='Path to the configuration file')
+    
+    args = parser.parse_args()
 
-    if not os.path.exists(folder):
-        print(f"The folder {folder} does not exist!")
-        return
+    # Load configuration
+    config = {}
+    if os.path.exists(args.config):
+        with open(args.config, 'r') as config_file:
+            config = json.load(config_file)
 
-    files = []
-    if scan_subdirectories:
-        for dirpath, _, filenames in os.walk(folder):
-            for f in filenames:
-                if not ext_filter or any(f.endswith(ext) for ext in ext_filter):
-                    files.append(os.path.join(dirpath, f))
+    custom_dict = config.get('custom_dictionary', [])
+    git_commit = config.get('git_commit', False)
+
+    files_to_check = []
+    if args.subdirs:
+        for dirpath, _, filenames in os.walk(args.path):
+            for file in filenames:
+                if file.endswith(tuple(args.extensions.split(','))):
+                    files_to_check.append(os.path.join(dirpath, file))
     else:
-        for f in os.listdir(folder):
-            if os.path.isfile(os.path.join(folder, f)) and (not ext_filter or any(f.endswith(ext) for ext in ext_filter)):
-                files.append(os.path.join(folder, f))
+        for file in os.listdir(args.path):
+            if os.path.isfile(os.path.join(args.path, file)) and file.endswith(tuple(args.extensions.split(','))):
+                files_to_check.append(os.path.join(args.path, file))
 
-    error_dict = {}
-    print("Checking spelling in files...")
-    for file in tqdm(files):
-        errors = check_spelling_in_file(file)
-        if errors:
-            error_dict[file] = errors
+    for file in tqdm(files_to_check, desc="Processing files"):
+        backup_file(file)  # Backup original file
+        check_and_correct_spelling(file, custom_dict)
 
-    num_checked = len(files)
-    num_errors = len(error_dict)
-
-    print(f"\n{num_checked} files were checked.")
-    if num_errors == 0:
-        print("No spelling errors found.")
-    else:
-        print(f"Found spelling errors in {num_errors} files.")
-        for file, errors in error_dict.items():
-            print(f"\nFile: {file}")
-            for word, (line_num, context) in errors.items():
-                print(f"Error: {word} (Line: {line_num}, Context: '{context}')")
-
-        log_folder = os.path.join(folder, 'log')
-        if not os.path.exists(log_folder):
-            os.mkdir(log_folder)
-
-        with open(os.path.join(log_folder, 'spelling_errors.log'), 'w', encoding="utf-8") as log:
-            for file, errors in error_dict.items():
-                log.write(f"File: {file}\n")
-                for word, (line_num, context) in errors.items():
-                    log.write(f"Error: {word} (Line: {line_num}, Context: '{context}')\n")
-                log.write("\n")
+    # Git Integration
+    if git_commit:
+        try:
+            repo = Repo(args.path)
+            repo.git.add(update=True)
+            repo.index.commit("Auto commit after spell check.")
+            print("Committed the changes to the repository.")
+        except:
+            print("Failed to commit changes. Ensure the directory is a git repository.")
 
 if __name__ == '__main__':
     main()
